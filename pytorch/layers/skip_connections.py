202eab24d326e1c76905a136d99c5f4c97a1ab12
Jaemin Cho <heythisischo@gmail.com>
2017-08-17 01:45:24 +0900

    Update Highway
    Optionally use identity for affine transformation

--- pytorch/layers/skip_connections.py
+++ pytorch/layers/skip_connections.py
@@ -2,14 +2,17 @@ from torch import nn
 from torch.nn import functional as F
 
 class Highway(nn.Module):
-    def __init__(self, size, num_layers, f=None):
+    def __init__(self, size, num_layers, identity=True, f=None):
 
         super(Highway, self).__init__()
 
         self.nonlinear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
         
-        self.linear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
+        self.identity = identity
         
+        if not self.identity:
+            self.linear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
+
         self.gate = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
 
         if f:
@@ -26,14 +29,18 @@ class Highway(nn.Module):
             
         Applies σ(x) ⨀ f(G(x)) + (1 - σ(x)) ⨀ Q(x)
         
-        linear: Q (affine transformation)
+        linear: Q (affine transformation) or x (Identity)
         nonlinear: f (non-linear tranformation) with G (affine transformation)
         gate: σ(x) (affine transformation) with sigmoid
         ⨀: element-wise multiplication
         """
-
-        for nonlinear, linear, gate in zip(self.nonlinear, self.linear, self.gate):
-            gate = F.sigmoid(gate(x))
-            x = gate * self.f(nonlinear(x)) + (1 - gate) * linear(x)
-
+        if self.identity:
+            for nonlinear, gate in zip(self.nonlinear, self.gate):
+                gate = F.sigmoid(gate(x))
+                x = gate * self.f(nonlinear(x)) + (1 - gate) * x
+        else:
+            for nonlinear, linear, gate in zip(self.nonlinear, self.linear, self.gate):
+                gate = F.sigmoid(gate(x))
+                x = gate * self.f(nonlinear(x)) + (1 - gate) * linear(x)
+                
         return x
