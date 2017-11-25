e712651354369eefa763eda234f1a758ed5b2d8b
Jaemin Cho <heythisischo@gmail.com>
2017-11-25 21:21:23 +0900

    Update reverse_order: in different dimensions

--- pytorch/shape_transform.py
+++ pytorch/shape_transform.py
@@ -1,12 +1,13 @@
 import torch
 from torch.autograd import Variable
 
-def reverse_order(tensor):
-    if isinstance(tensor, torch.Tensor):
-        idx = [i for i in range(tensor.size(0)-1, -1, -1)]
+def reverse_order(tensor, dim=0):
+    """Reverse Tensor or Variable along given dimension"""
+    if isinstance(tensor, torch.Tensor) or isinstance(tensor, torch.LongTensor):
+        idx = [i for i in range(tensor.size(dim)-1, -1, -1)]
         idx = torch.LongTensor(idx)
-        inverted_tensor = tensor.index_select(0, idx)
+        inverted_tensor = tensor.index_select(dim, idx)
         return inverted_tensor
     elif isinstance(tensor, Variable):
-        tensor.data = reverse_order(tensor.data)
+        tensor.data = reverse_order(tensor.data, dim=dim)
         return tensor
