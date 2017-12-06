4ee0136f460e0c56fd735e0386cbb73e39f1b958
Jaemin Cho <heythisischo@gmail.com>
2017-12-06 16:14:50 +0900

    Update network_inspect.py

--- pytorch/network_inspect.py
+++ pytorch/network_inspect.py
@@ -1,6 +1,16 @@
-def count_parameters(module):
+
+def count_params(module):
     """Print total number of parameteres in given module"""
     num_params = 0
     for param in module.parameters():
+        num_params += param.numel()
+    print('Total number of parameters: %d' % num_params)
+
+def view_params(module):
+    """Overview all parameters of given model (model name, param size) + # of total params""""
+    print('Overview Parameters')
+    num_params = 0
+    for name, param in self.model.named_parameters():
+        print('\t' + name + '\t', list(param.size()))
         num_params += param.numel()
     print('Total number of parameters: %d' % num_params)
